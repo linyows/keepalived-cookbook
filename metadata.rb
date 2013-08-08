b3842223991252e328c78825dae5d5f32aa30034
linyows <linyows@gmail.com>
2013-08-08 14:45:01 +0900

    bump version to 0.2.1

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'linyows@gmail.com'
 license 'All rights reserved'
 description 'Installs/Configures keepalived'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.2.0'
+version '0.2.1'
 
 %w(centos redhat fedora ubuntu debian).each { |os| supports os }

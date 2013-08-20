7e6b867cc7d0648c803abb4a75960f35227a5a85
linyows <linyows@gmail.com>
2013-08-20 23:38:57 +0900

    add indent and change license

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,11 @@
-name 'keepalived'
-maintainer 'linyows'
+name             'keepalived'
+maintainer       'linyows'
 maintainer_email 'linyows@gmail.com'
-license 'All rights reserved'
-description 'Installs/Configures keepalived'
+license          'MIT'
+description      'Installs/Configures keepalived'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.2.1'
+version          '0.2.1'
+
+recipe 'keepalived', 'Installs and configures keepalived'
 
 %w(centos redhat fedora ubuntu debian).each { |os| supports os }

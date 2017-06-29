b1aa66b84d2f716a218e4a5b7dd8c03aa6632657
pyama86 <pyama@pepabo.com>
2017-06-29 13:49:23 +0900

    add files uniq

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,7 +7,9 @@ files = Array(node['keepalived']['check_scripts']).map { |k,v|
   Chef::Keepalived.outside_conf_file_name(:vrrp_instance, k)
 } + Array(node['keepalived']['virtual_servers']).map { |k,v|
   Chef::Keepalived.outside_conf_file_name(:virtual_server, k)
-} || node['keepalived']['include_files']
+}
+
+files << node['keepalived']['include_files']
 
 keepalived node['keepalived']['router_id'] do
   action :enable
@@ -17,7 +19,7 @@ keepalived node['keepalived']['router_id'] do
   notification_email_from node['keepalived']['notification_email_from']
   smtp_server node['keepalived']['smtp_server']
   smtp_connect_timeout node['keepalived']['smtp_connect_timeout']
-  include_files files
+  include_files files.flatten.uniq
 end
 
 # nothing to do if attributes nothing set

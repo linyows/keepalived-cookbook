e2ff91863a8fef6f8a30b3623a4c07d0a521293a
linyows <linyows@gmail.com>
2017-07-08 17:45:50 +0900

    add provider for sync group

---
+++ providers/vrrp_sync_group.rb
@@ -0,0 +1,45 @@
+# Cookbook Name:: keepalived
+# Provider:: vrrp_sync_group
+
+include Chef::Mixin::Keepalived
+
+def whyrun_supported?
+  true
+end
+
+action :create do
+  converge_by "Run #{new_resource}" do
+    keepalived_outside_confs "init for #{new_resource.name} vrrp_sync_group" do
+      action :init
+    end
+
+    options = %w(
+      notify_backup
+      notify_fault
+      notify_master
+    ).each_with_object({}) do |attr, result|
+      if new_resource.respond_to?(attr) && !new_resource.send(attr.to_sym).nil?
+        result[attr] = new_resource.send(attr.to_sym)
+      end
+    end
+
+    file_name = new_resource.file_name || outside_conf_file_name(new_resource.name)
+
+    r = template file_name do
+      path "#{outside_conf_dir_path}/#{file_name}"
+      source 'vrrp_sync_group.conf.erb'
+      cookbook 'keepalived'
+      owner 'root'
+      group 'root'
+      mode 0644
+      variables(
+        'name' => new_resource.name,
+        'group' => new_resource.group,
+        'options' => options
+      )
+      notifies :restart, 'service[keepalived]'
+    end
+
+    new_resource.updated_by_last_action(r.updated_by_last_action?)
+  end
+end

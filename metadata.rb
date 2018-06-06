f55ea870711237ca62990da1ee406f202c3fd00e
linyows <linyows@gmail.com>
2018-06-06 22:38:47 +0900

    fix for FC066 and more

--- metadata.rb
+++ metadata.rb
@@ -5,8 +5,12 @@ license          'MIT'
 description      'Installs/Configures keepalived'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.7.2'
+chef_version '>= 12.5' if respond_to?(:chef_version)
 
 recipe 'keepalived', 'Installs and configures keepalived'
 recipe 'keepalived::disabled', 'disable keepalived'
 
 %w(centos redhat fedora ubuntu debian).each { |os| supports os }
+
+source_url 'https://github.com/linyows/keepalived-cookbook'
+issues_url 'https://github.com/linyows/keepalived-cookbook/issues'
